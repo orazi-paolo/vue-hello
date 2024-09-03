687df0f0fbdf10d36db3002e2e4d00c6df677685
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-03 15:58:36 +0200

    FINISH + BONUS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            message: 'Primo messaggio in VUE!',
+            img: "https://c4.wallpaperflare.com/wallpaper/329/377/202/kingdom-hearts-kingdom-hearts-iii-hd-wallpaper-preview.jpg"
+        }
+    }
+}).mount('#app');
